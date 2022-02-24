ef28874934dd43ce75b3f8a55d70b42d364e05b0
Jewoo <jeus0630@designfever.com>
2022-02-24 20:41:55 +0900

    Update: userSlice.js
    createAsyncThunk(), extraReducers 사용

--- initialTemplate/src/redux/userSlice.js
+++ initialTemplate/src/redux/userSlice.js
@@ -1,4 +1,18 @@
-import {createSlice} from "@reduxjs/toolkit"
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit"
+
+export const updateUser = createAsyncThunk("users/update", async (user) => {
+    const res = await fetch("http://localhost:8080/api/users/1/");
+    const data = await res.json();
+
+    return data;
+})
+
+export const deleteUser = createAsyncThunk("users/delete", async (user) => {
+    const res = await fetch("http://localhost:8080/api/users/1/");
+    const data = await res.json();
+
+    return data;
+})
 
 export const userSlice = createSlice({
     name: "user",
@@ -14,8 +28,33 @@ export const userSlice = createSlice({
                 email: action.payload.email
             }
         },
-        remove: (state) => (state = {}),
-        add: 
+        remove: (state) => (state = {})
+    },
+    extraReducers:{
+        [updateUser.pending] : (state) => {
+            state.pending = true;
+            state.error = false;
+        },
+        [updateUser.fulfilled] : (state,action) => {
+            state.pending = false;
+            state.userInfo = action.payload;
+        },
+        [updateUser.rejected] : (state) => {
+            state.pending = false;
+            state.error = true;
+        },
+        [deleteUser.pending] : (state) => {
+            state.pending = true;
+            state.error = false;
+        },
+        [deleteUser.fulfilled] : (state,action) => {
+            state.pending = false;
+            state.userInfo = action.payload;
+        },
+        [deleteUser.rejected] : (state) => {
+            state.pending = false;
+            state.error = true;
+        }
     }
 })
 

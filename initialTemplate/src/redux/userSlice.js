3035babec1e521f13359a3698bb0da9c1007bd66
Jewoo <jeus0630@designfever.com>
2022-02-24 14:59:25 +0900

    docs : store.js userSlice.js
    redux/toolkit initiallize

---
+++ initialTemplate/src/redux/userSlice.js
@@ -0,0 +1,14 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState: {
+        name: "john",
+        email: "john@gmail.com"
+    },
+    reducers: {
+        update: (sate,action) => {
+            
+        }
+    }
+})
